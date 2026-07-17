329662aafed078adc21cead1be6806195509d0f5
SAVINDUGUNATHILAKA <savindugunathilaka08@gmail.com>
2026-07-18 03:50:23 +0530

    Refactor app.js to comment out Customer class and customer objects; update index.html to add spacing and maintain structure

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-class Customer{
-    name;
-    age;
-    address;
+// class Customer {
+//     name;
+//     age;
+//     address;
 
-    constructor(name,age,address){
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-}
+//     constructor(name, age, address) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
+// }
 
-let customer1 = new Customer("Sharadha", 20, "Colombo");
-console.log(customer1);
+// let customer1 = new Customer("Sharadha", 20, "Colombo");
+// console.log(customer1);
 
-let customer2 = {
-    name: "John",
-    age: 25,
-    address: "New York",
-    salary: 5000
-};
-console.log(customer2.address);
-console.log(customer2.age);
-console.log(customer2.name);
-console.log(customer2.salary);
+// let customer2 = {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//     salary: 5000
+// };
+
+// console.log(customer2.address);
+// console.log(customer2.age);
+// console.log(customer2.name);
+// console.log(customer2.salary);
+
+
+
+//Document Object Model (DOM)
+
+console.log(document.documentURI);
+document.write("<h2> Hello, Me! </h2>");
+
