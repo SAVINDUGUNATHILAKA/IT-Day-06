6c7808c30e60b6cbd241b513ef862eff5eaea4bc
SAVINDUGUNATHILAKA <savindugunathilaka08@gmail.com>
2026-07-18 02:34:51 +0530

    Add Customer class and instantiate customer objects

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hi")
+class Customer{
+    name;
+    age;
+    address;
+
+    constructor(name,age,address){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
+
+let customer1 = new Customer("Sharadha", 20, "Colombo");
+console.log(customer1);
+
+let customer2 = {
+    name: "John",
+    age: 25,
+    address: "New York"
+};
+console.log(customer2);
+
